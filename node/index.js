fd8d2e79e81d6a3de3e43fa8632dc676b0a3ad5b
Mike Oliveira <mike.oliveiracosta@outlook.com>
2025-02-02 01:44:17 -0300

    feat: adicionado projeto rodando em 3 dockerfile diferentes e funcionando sob uma mesma network

--- node/index.js
+++ node/index.js
@@ -3,24 +3,66 @@ const app = express()
 
 const port = 3000
 
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database:'nodedb'
-// };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
 
-// const sql = `INSERT INTO people(name) values('Wesley')`
-// connection.query(sql)
-// connection.end()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(config)
 
+const insertPerson = async () => {
+    const sql = `INSERT INTO people(name) values('Mike')`
+    try {
+        await connection.promise().query(sql)
+        console.log('Pessoa inserida com sucesso')
+    } catch (error) {
+        console.error('Erro ao inserir pessoa:', error)
+    }
+}
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+app.get('/', async (req,res) => {
+    
+
+    try {
+        // Usando Promises
+        const [rows] = await connection.promise().query('SELECT * FROM people');
+        
+        let html = '<h1>Full Cycle Rocks!</h1>';
+
+        html += '<table border="1" style="border: 1px solid;">';
+        html+= `<tr>
+        <td>
+        ID
+        </td>
+        <td>
+        Name
+        </td>
+                </tr>`
+        rows.forEach(person => {
+            html += 
+            `<tr>
+            <td>${person.id}</td>
+            <td>${person.name}</td>
+            </tr>`; 
+        });
+        
+        html += '</table>';
+        res.send(html)
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
 })
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
+    insertPerson()
+})
+
+// Tratamento adequado do fechamento da conexão
+process.on('SIGTERM', () => {
+    connection.end()
+    process.exit(0)
 })
